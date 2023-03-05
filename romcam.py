63e3c990bcd33d174763cfd77da4f984f2683e90
pata98 <yoopata@postech.ac.kr>
2023-03-05 21:08:33 +0900

    init commit

--- romcam.py
+++ romcam.py
@@ -1,3 +1,10 @@
-print('Hello world!')
-print('Good day, commander')
-print('Give me something to burn')
+# import packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+# initialize camera
+camera = PiCamera()
+rawCap = PiRGBArray(camera)
+time.sleep(0.1)
