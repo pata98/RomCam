3e0502b73e343d0952a5cc2903c12aebba7a1fdc
pata98 <yoopata@postech.ac.kr>
2023-03-07 01:42:05 +0900

    2

--- romcam.py
+++ romcam.py
@@ -2,13 +2,11 @@
 #from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-import sleep
 
 # initialize camera
 #camera = PiCamera()
-sleep(0.1)
+time.sleep(0.1)
 #camera.start_preview()
-sleep(10)
-
+time.sleep(10)
 #camera.capture('image1.jpg')
 #camera.stop_preview()
