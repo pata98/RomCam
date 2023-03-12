8928638d72a8885d81594fa4679a39b95d8f2483
pata98 <yoopata@postech.ac.kr>
2023-03-12 16:29:51 +0900

    add gpio, remove romcam2

--- romcam.py
+++ romcam.py
@@ -1,12 +1,31 @@
 # import packages
 #from picamera.array import PiRGBArray
+from RPi.GPIO as GPIO
 from picamera import PiCamera
 import time
 
-# initialize camera
+#### DEFINE PIN NUMBER ####
+shutter = 1
+led = 2
+
+### init
+# Camera
 camera = PiCamera()
-time.sleep(0.1)
-camera.start_preview()
-time.sleep(2)
-camera.capture('image1.jpg')
-camera.stop_preview()
+# GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(shutter, GPIO.IN, pull_up_down=GPIO.PUD.UP)
+
+# main
+while True:
+    if GPIO.input(shutter)
+        time.sleep(0.1)
+    else:
+        camera.capture('image1.jpg')
+        GPIO.output(led, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(led, GPIO.LOW)
+        
+
+# Exit, clear GPIO settings
+GPIO.clearup()
